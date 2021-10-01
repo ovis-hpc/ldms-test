7a8a1eb68c939623a03f9c54eee5b59483055896
Narate Taerat <narate@ogc.us>
2021-09-30 23:45:14 -0500

    Add cygnus-weekly.sh test script

---
+++ cygnus-weekly.sh
@@ -0,0 +1,265 @@
+#!/bin/bash
+#
+# WARNINGS:
+# - This script uses `sudo` to purge /opt/ovis/ directory.
+# - This script installs OVIS-4 at /opt/ovis/ on the host.
+# - This script kill all running virtual clusters.
+# - This script make a soft link in /etc/ld.so.conf.d/
+# - This script purge $WORK_DIR/
+#
+# DESCRIPTION
+# -----------
+# This script is meant to be run on cygnus cluster at OGC. The following is an
+# overview of what this script does:
+# - Kill all running virtual clusters
+# - Checkout OVIS-4 and SOS from github
+# - (sudo) purge /opt/ovis on the host
+# - build and (sudo) install SOS and OVIS-4 in /opt/ovis on the host
+# - run `direct_*_test` test cases
+# - run containerized test cases
+#
+# REQUIREMENTS
+# ------------
+# - you must be able to `sudo`
+# - if you use singularity, make sure that you build and own the singularity
+#   image, and you have to be in the /etc/subuid and /etc/subgid list. See
+#   details in "Singularity Cluster Set" section in README.md.
+
+LOG() {
+	echo $( date +"%F %T" ) "$@"
+}
+export -f LOG
+
+ERROR() {
+	LOG "ERROR:" "$@"
+}
+export -f ERROR
+
+INFO() {
+	LOG "INFO:" "$@"
+}
+export -f INFO
+
+assert() {
+	"$@" || {
+		ERROR "cmd: '$*' failed"
+		exit -1
+	}
+}
+export -f assert
+
+SCRIPT_DIR=$(realpath $(dirname $0))
+
+WORK_DIR=${WORK_DIR:-/mnt/cygnus/data/$(date +"%F-%H%M%S")}
+WORK_DIR=$(realpath ${WORK_DIR})
+INFO "Purging workdir: ${WORK_DIR}"
+rm -rf ${WORK_DIR}
+DATA_ROOT=${WORK_DIR}/data
+assert mkdir -p ${DATA_ROOT}
+
+LOG=${LOG:-${WORK_DIR}/cygnus-weekly.log}
+D=$(dirname $LOG)
+assert mkdir -p $D
+
+PREFIX=/opt/ovis
+
+MAESTRO_COMMIT=${MAESTRO_COMMIT:-78af06a}
+
+SOS_BUILD_OPTS=(
+	--prefix=${PREFIX}
+	--enable-python
+	CFLAGS="-Wall -Werror -O0 -ggdb3"
+)
+
+OVIS_BUILD_OPTS=(
+	--prefix=${PREFIX}
+	--enable-python
+
+	# test stuff
+	--enable-ldms-test
+	--enable-zaptest
+	--enable-test_sampler
+	--enable-list_sampler
+
+	--enable-munge
+
+	--enable-sos
+	--with-sos=${PREFIX}
+
+	# xprt
+	--enable-rdma
+	--enable-fabric
+	--with-libfabric=/usr
+
+	# app stuff
+	--enable-store-app
+	--enable-app-sampler
+
+	# etc and doc
+	--enable-etc
+	--enable-doc
+	--enable-doc-man
+
+	CFLAGS="-Wall -Werror -O0 -ggdb3"
+)
+
+#### subshell that print to both stdout and log file ####
+{
+INFO "WORK_DIR: ${WORK_DIR}"
+INFO "LOG: ${LOG}"
+
+
+set -e
+# remove existing clusters
+./remove_cluster --all
+
+pushd ${WORK_DIR}
+INFO "== Checking out SOS =="
+git clone https://github.com/ovis-hpc/sos
+INFO "== Checking out OVIS =="
+git clone https://github.com/ovis-hpc/ovis
+INFO "== Checkout maestro =="
+git clone https://github.com/ovis-hpc/maestro
+[[ -z "${MAESTRO_COMMIT}" ]] || {
+	INFO "checking out maestro commit id: ${MAESTRO_COMMIT}"
+	pushd maestro
+	git checkout ${MAESTRO_COMMIT}
+	popd
+}
+
+NEW_GIT_SHA=$( cd ovis && git rev-parse HEAD )
+OLD_GIT_SHA=$( [[ -x /opt/ovis/sbin/ldmsd ]] && {
+		/opt/ovis/sbin/ldmsd -V | grep git | sed 's/git-SHA: //'
+	} || echo "" )
+
+INFO "NEW_GIT_SHA: ${NEW_GIT_SHA}"
+INFO "OLD_GIT_SHA: ${OLD_GIT_SHA}"
+
+if [[ "$NEW_GIT_SHA" != "$OLD_GIT_SHA" ]]; then
+	INFO "Purging /opt/ovis/"
+	sudo rm -rf /opt/ovis/* # we want to keep the directory, just purge stuff inside
+
+	INFO "== Building/Installing SOS =="
+	pushd sos
+	./autogen.sh 2>&1
+	mkdir -p build
+	pushd build
+	../configure "${SOS_BUILD_OPTS[@]}" 2>&1
+	make
+	sudo make install
+	popd # back to sos
+	popd # back to ${WORK_DIR}
+
+	INFO "== Building/Installing OVIS =="
+	pushd ovis
+	./autogen.sh 2>&1
+	mkdir -p build
+	pushd build
+	../configure "${OVIS_BUILD_OPTS[@]}" 2>&1
+	make
+	sudo make install
+	popd # back to ovis
+	popd # back to ${WORK_DIR}
+
+	INFO "== Installing maestro =="
+	sudo cp maestro/*.py maestro/maestro* ${PREFIX}/bin
+else
+	INFO "skip building because GIT SHA has not changed: ${OLD_GIT_SHA}"
+fi
+
+set +e
+INFO "==== OVIS+SOS Installation Completed ===="
+
+
+INFO "==== Start batch testing ===="
+
+pushd ${SCRIPT_DIR} # it is easier to call scripts from the script dir
+
+TEST_OPTS=(
+	--prefix ${PREFIX}
+	--src ${WORK_DIR}
+)
+
+[[ -z "${FAIL_FAST}" ]] || set -e
+
+declare -A RCS
+
+for T in direct_ldms_ls_conn_test direct_prdcr_subscribe_test; do
+	INFO "======== ${T} ========"
+	CMD="python3 ${T} ${TEST_OPTS[@]} --data_root ${DATA_ROOT}/${T}"
+	INFO "CMD: ${CMD}"
+	${CMD}
+	RCS["$T"]=$?
+	INFO "----------------------------------------------"
+done 2>&1
+
+[[ -z "${SKIP_PAPI}" ]] && {
+	PAPI_LIST=(
+		agg_slurm_test
+		papi_sampler_test
+		papi_store_test
+		store_app_test
+		syspapi_test
+	)
+} || {
+	PAPI_LIST=
+}
+
+LIST=(
+${PAPI_LIST[*]}
+agg_test
+failover_test
+ldmsd_auth_ovis_test
+ldmsd_auth_test
+ldmsd_ctrl_test
+ldmsd_stream_test
+maestro_cfg_test
+mt-slurm-test
+ovis_ev_test
+prdcr_subscribe_test
+set_array_test
+setgroup_test
+slurm_stream_test
+spank_notifier_test
+ldms_list_test
+quick_set_add_rm_test
+set_array_hang_test
+ldmsd_autointerval_test
+)
+
+for T in ${LIST[*]}; do
+	INFO "======== ${T} ========"
+	CMD="python3 ${T} ${TEST_OPTS[@]} --data_root ${DATA_ROOT}/${T}"
+	INFO "CMD: ${CMD}"
+	${CMD}
+	RCS["$T"]=$?
+	sleep 10 # allow some clean-up break between tests
+	INFO "----------------------------------------------"
+	./remove_cluster --all
+	rm -rf ${DATA_ROOT}/${T}
+done 2>&1
+
+export RED='\033[01;31m'
+export GREEN='\033[01;32m'
+export RESET='\033[0m'
+INFO "==== Summary ===="
+N=${#RCS[*]}
+PASSED=0
+FAILED=0
+for K in "${!RCS[@]}"; do
+	V=${RCS["$K"]}
+	if (( $V == 0 )); then
+		PASSED=$((PASSED+1))
+		V="${GREEN}PASSED${RESET}"
+	else
+		FAILED=$((FAILED+1))
+		V="${RED}FAILED${RESET}"
+	fi
+	echo -e "${K}: ${V}"
+done
+echo "------------------------------------------"
+echo -e "Total tests: $N"
+echo -e "Total passed: $PASSED"
+echo -e "Total failed: $FAILED"
+echo "------------------------------------------"
+} | tee ${LOG}
