6d4ab659b7a8146b1fcdf7d8b46301cf6e07b3c0
Narate Taerat <narate@ogc.us>
2023-08-30 10:12:39 -0500

    Allow LDMSD_PORT override in 'python/pypubsub.py'

--- python/pypubsub.py
+++ python/pypubsub.py
@@ -2,6 +2,8 @@
 #
 # pypubsub.py - LDMS stream publisher / subscriber interactive script for using
 #               in ldmsd_stream_test
+import os
+
 from ovis_ldms import ldms
 import socket
 
@@ -9,5 +11,7 @@ ldms.init(128 * 1024 * 1024)
 
 scli = ldms.StreamClient('.*', is_regex = True)
 
+PORT = int(os.environ.get("LDMSD_PORT", "10000"))
+
 r = ldms.Xprt("sock")
-r.connect(host=socket.gethostname(), port=10000)
+r.connect(host=socket.gethostname(), port=PORT)
