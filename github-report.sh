8327c35d5dc35df63caddab8574fe1549e3cd639
Narate Taerat <narate@ogc.us>
2024-10-16 13:20:01 -0500

    Add 'REPORT_BRANCH' support
    Add REPORT_BRANCH support and update cygnus-weekly-cont-config.sh.example

--- github-report.sh
+++ github-report.sh
@@ -11,6 +11,8 @@ SCRIPT_DIR=$(realpath $(dirname $0))
 
 pushd "${SCRIPT_DIR}"
 
+[[ -f cygnus-weekly-cont-config.sh ]] && . cygnus-weekly-cont-config.sh
+
 LOG() {
 	echo $( date +"%F %T" ) "$@"
 }
@@ -43,13 +45,19 @@ export -f assert
 	exit -1
 }
 
+REPORT_BRANCH=${REPORT_BRANCH:-master}
+
 [[ -d "weekly-report" ]] || {
 	# clone the weekly-report repo
 	INFO "weekly-report directory not found"
 	INFO "cloning weekly-report from github"
 	# the current working directory is SCRIPT_DIR
 	set -e
-	git clone git@github.com:ldms-test/weekly-report
+	mkdir -p weekly-report
+	pushd weekly-report
+	git init .
+	git remote add origin git@github.com:ldms-test/weekly-report
+	popd
 }
 
 # Check if the tests have skipped due to unchanged SHA
@@ -60,8 +68,15 @@ if grep 'Skip the test. SHA does not change:' ${WORK_DIR}/cygnus-weekly.log >/de
 fi
 
 pushd weekly-report
-git fetch
-git reset --hard origin/master
+
+git branch -m ${REPORT_BRANCH}
+BR=$(git ls-remote origin ${REPORT_BRANCH})
+if [[ -n "${BR}" ]]; then
+	# branch existed
+	git fetch origin ${REPORT_BRANCH}
+	git reset --hard FETCH_HEAD
+fi
+
 cp ${WORK_DIR}/cygnus-weekly.log test-all.log
 if [[ -d ${WORK_DIR}/test-log ]]; then
 	if [[ -d test-log ]]; then
@@ -90,12 +105,25 @@ else
 	COLOR='red'
 fi
 
+# figuring out the commit-id
+if [[ -z "${OVIS_GIT_SHA}" ]]; then
+	A=($(grep -m 1 -E "INFO *commit-id:" test-all.log))
+	if [[ "${#A[@]}" -gt 0 ]] ; then
+		OVIS_GIT_SHA=${A[-1]}
+	fi
+fi
+
+if [[ -z "${LDMS_TEST_GIT_SHA}" ]]; then
+	pushd ${SCRIPT_DIR}
+	LDMS_TEST_GIT_SHA=$(git rev-parse HEAD)
+	popd
+fi
 
 # caller set OVIS_GIT_SHA
 OVIS_GIT_SHA_SHORT=${OVIS_GIT_SHA:0:7} # abcdef0
 LDMS_TEST_GIT_SHA_SHORT=${LDMS_TEST_GIT_SHA:0:7}
 
-LABEL="weekly test on ovis:${OVIS_GIT_SHA_SHORT} and ldms-test:${LDMS_TEST_GIT_SHA_SHORT}"
+LABEL="weekly test on ldms branch ${OVIS_BRANCH}:${OVIS_GIT_SHA_SHORT} and ldms-test:${LDMS_TEST_GIT_SHA_SHORT}"
 
 cat > status.json <<EOF
 {
@@ -105,6 +133,19 @@ cat > status.json <<EOF
   "color": "${COLOR}"
 }
 EOF
+git add status.json
+
+cat > README.md <<EOF
+[![status](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/ldms-test/weekly-report/${REPORT_BRANCH}/status.json)](https://github.com/ldms-test/weekly-report/blob/${REPORT_BRANCH}/test-all.log)
+
+Weekly Test Report
+==================
+
+This repository hosts reports of the weekly test of \`${REPORT_BRANCH}\` and the
+shields-io endpoint badge data.
+EOF
+git add README.md
+
 git commit -a -m  "$(basename ${WORK_DIR})"
-git push -f origin
+git push -f origin ${REPORT_BRANCH}
 popd # back to SCRIPT_DIR
